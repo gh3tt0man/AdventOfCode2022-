31de1a4825c611b4700c449c6a273a9d608ba463
gh3tt0man <minchevrosen@gmail.com>
2023-04-06 16:59:48 +0300

    Day5

--- Day5/containers.py
+++ Day5/containers.py
@@ -5,23 +5,81 @@ dir = os.path.dirname(sys.argv[0])
 score=0
 with open(f'{dir}/input.txt','r') as f:
     for line in f:
-        listOfLists =[[''] for i in range(int((len(line)-1)/3))]
+        listOfLists =[[''] for i in range(int(len(line)))]
         break
-
+moveMode = False
 with open(f'{dir}/input.txt','r') as f:
     for i,line in enumerate(f):
-        print(len(line))
+        if len(line) < 3:
+            moveMode = True
+            cleanedList = []            
+
+            for b in listOfLists:
+                    if b != ['']:
+                        b = [c for c in b if c]
+                        cleanedList.append(b)
+            continue
         arr = [""]*(len(line)-1)
-        for j,char in enumerate(line):
-            if char == 'm':
-                quit()
-            if char == '\n':
-                continue
-            if char == ' ' or char == '[' or char == "]":
-                continue
-            listOfLists[int((j-1)/3)].append(char)
-print(listOfLists)
+        if not moveMode:
+            for j,char in enumerate(line):
+                if char == '\n':
+                    continue
+                if char == ' ' or char == '[' or char == "]":
+                    continue
+                listOfLists[int(j)].append(char)
+
+        if moveMode:
+            move = [int(x) for x in line.strip().split(' ') if x.isdigit()]
+            #originator list
+            rev = cleanedList[move[1]-1][:move[0]]
+            rev.reverse()           
+
+            for k in range(len(rev)-1,-1,-1):
+                cleanedList[move[2]-1].insert(0,rev[k])
+            del cleanedList[move[1]-1][:move[0]]
+
+            
+print(''.join([y[0] for y in cleanedList]))
 
 
-#remove empty lists from list of lists
+#Part 2
+#only change is removal of .reverse()
+dir = os.path.dirname(sys.argv[0])
+score=0
+with open(f'{dir}/input.txt','r') as f:
+    for line in f:
+        listOfLists =[[''] for i in range(int(len(line)))]
+        break
+moveMode = False
+with open(f'{dir}/input.txt','r') as f:
+    for i,line in enumerate(f):
+        if len(line) < 3:
+            moveMode = True
+            cleanedList = []            
 
+            for b in listOfLists:
+                    if b != ['']:
+                        b = [c for c in b if c]
+                        cleanedList.append(b)
+            continue
+        arr = [""]*(len(line)-1)
+        if not moveMode:
+            for j,char in enumerate(line):
+                if char == '\n':
+                    continue
+                if char == ' ' or char == '[' or char == "]":
+                    continue
+                listOfLists[int(j)].append(char)
+
+        if moveMode:
+            move = [int(x) for x in line.strip().split(' ') if x.isdigit()]
+            #originator list
+            rev = cleanedList[move[1]-1][:move[0]]
+            #rev.reverse()           
+
+            for k in range(len(rev)-1,-1,-1):
+                cleanedList[move[2]-1].insert(0,rev[k])
+            del cleanedList[move[1]-1][:move[0]]
+
+            
+print(''.join([y[0] for y in cleanedList]))
