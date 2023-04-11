ce6d3465c3ccc620d77df83f8835548fa73facea
gh3tt0man <minchevrosen@gmail.com>
2023-04-11 19:28:53 +0300

    Day10 improvements

--- Day10/signalStrength.py
+++ Day10/signalStrength.py
@@ -1,118 +1,74 @@
 # Part 1
-
-##Make the tail positions 1s , and mark with 1 where the tail has been
 import os
 import sys
 import pandas as pd
 dir = os.path.dirname(sys.argv[0])
 
-sum=0
-CPUcycle=0
-X=1
+def update_sum_and_print(cycle, x, total):
+    if cycle % 40 == 20:
+        total += cycle * x
+        print(f'cycle: {cycle}, sum: {total}, X: {x}')
+    return total
+
+sum = 0
+CPUcycle = 0
+X = 1
 
 with open(f'{dir}/input.txt', 'r') as f:
     for line in f:
         line = line.strip()
         if line == 'noop':
-            CPUcycle+=1
-            if CPUcycle in [20,60,100,140,180,220]:            
-                sum+=CPUcycle*X            
-                print('cycle:',CPUcycle,'sum:',sum,'X:',X)                
-            continue        
-        CPUcycle+=1
-                        
-        if CPUcycle in [20,60,100,140,180,220]:            
-            sum+=CPUcycle*X            
-            print('cycle:',CPUcycle,'sum:',sum,'X:',X)
-        CPUcycle+=1
-        if CPUcycle in [20,60,100,140,180,220]:            
-            sum+=CPUcycle*X
-            print('cycle:',CPUcycle,'sum:',sum,'X:',X)
-        value = int(line.split(' ')[1])              
-        
-              
-        X+=value
+            CPUcycle += 1
+            sum = update_sum_and_print(CPUcycle, X, sum)
+            continue
+        CPUcycle += 1
+        sum = update_sum_and_print(CPUcycle, X, sum)
+        CPUcycle += 1
+        sum = update_sum_and_print(CPUcycle, X, sum)
+        value = int(line.split(' ')[1])
+
+        X += value
 print(sum)
 
-#Part2
+# Part2
 
-CRT = [['.' for _ in range(0,40)] for _ in range(0,6)]
+CRT = [['.' for _ in range(0, 40)] for _ in range(0, 6)]
+row = 0
 
-def update_crt(row,CPUcycle,X):
-    if row == 0:
-        dx = CPUcycle-1-X
-    else:
-        dx = (CPUcycle-1-40*row)-X
-    if dx in range(-1,2):
-        CRT[row][CPUcycle-1-row*40]='#'
-    if CPUcycle%40 == 0:
-        row+=1    
+def update_crt(CPUcycle, X, row):
+    offset_cycle = CPUcycle - 1 - row * 40
+    dx = offset_cycle - X
+    if -2 < dx < 2:
+        CRT[row][offset_cycle] = '#'
+    if offset_cycle == 39:
+        row += 1
+    return row
 
-CPUcycle=0
-X=1
-row=0
+
+CPUcycle = 0
+X = 1
+
 with open(f'{dir}/input.txt', 'r') as f:
     for line in f:
         line = line.strip()
         if line == 'noop':
-            CPUcycle+=1
-            print('CPU:',CPUcycle,'row:',row,'X:',X)
+            CPUcycle += 1
+            # print('CPU:',CPUcycle,'row:',row,'X:',X)
+            row = update_crt(CPUcycle, X, row)
+            continue
 
-            if row == 0:
-                dx = CPUcycle-1-X
-            else:
-               dx = (CPUcycle-1-40*row)-X                        
-            if dx in range(-1,2):
-                CRT[row][CPUcycle-1-row*40]='#'
-            if CPUcycle%40 == 0:
-                row+=1                
-            continue                
+        CPUcycle += 1
+        # print('CPU:',CPUcycle,'row:',row,'X:',X)
+        row = update_crt(CPUcycle, X, row)
 
-        CPUcycle+=1
-        print('CPU:',CPUcycle,'row:',row,'X:',X)
+        CPUcycle += 1
+        # print('CPU:',CPUcycle,'row:',row,'X:',X)
+        row = update_crt(CPUcycle, X, row)
 
-        if row == 0:
-            dx = CPUcycle-1-X
-        else:
-            dx = (CPUcycle-1-40*row)-X
-        if dx in range(-1,2):
-            CRT[row][CPUcycle-1-row*40]='#'
-        if CPUcycle%40 == 0:
-            row+=1
+        value = int(line.split(' ')[1])
 
-        CPUcycle+=1
-        print('CPU:',CPUcycle,'row:',row,'X:',X)
-
-        if row == 0:
-            dx = CPUcycle-1-X
-        else:
-            dx = (CPUcycle-1-40*row)-X
-        if dx in range(-1,2):
-            CRT[row][CPUcycle-1-row*40]='#' 
-        if CPUcycle%40 == 0:
-            row+=1
-
-        value = int(line.split(' ')[1])              
-        
-              
-        X+=value
-
-
-
+        X += value
 
 df = pd.DataFrame(CRT)
 # Display the DataFrame
 print(df)
-'''
-cpu 18  X 20
-18 - 20 = -2 NOK
-
-cpu 19 X 20 = -1 OK
-
-CPU 21 X 20 = 1 OK
-CPU 22 X 20 = 2 NOK
-
-CPU 70 X 71  = oK pixel 70#
-
-70/2
-'''
